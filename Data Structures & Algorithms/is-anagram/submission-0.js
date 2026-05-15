4b27324a8ee3588a31e48cbcb2254c5979f297cf
Slim <152201677+iAMslim@users.noreply.github.com>
2026-05-14 19:37:49 -0500

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const count = new Array(26).fill(0);
+
+        for (let i = 0; i < s.length; i++) {
+            count[s.charCodeAt(i) - 97]++;
+            count[t.charCodeAt(i) - 97]--;
+        }
+
+        return count.every(val => val === 0);
+    }
+}
